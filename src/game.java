4fe4e6ec698f6e947252219fc53cd836d8253ea8
YaroslavZashipas <zasipasaroslav@gmail.com>
2026-01-30 13:24:02 +0100

    Chess
    Signed-off-by: YaroslavZashipas <zasipasaroslav@gmail.com>

--- src/game.java
+++ src/game.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class game {
 
     public String[][] board = {
@@ -13,24 +11,29 @@ public class game {
             {"R","N","B","Q","K","B","N","R"}
     };
 
+
     public void move(String from, String to) {
         int fx = from.charAt(0) - 'a';
         int fy = 8 - (from.charAt(1) - '0');
         int tx = to.charAt(0) - 'a';
         int ty = 8 - (to.charAt(1) - '0');
 
+
         board[ty][tx] = board[fy][fx];
         board[fy][fx] = "";
     }
 
-    public void startConsole() {
-        Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("Ход (e2 e4): ");
-            String from = sc.next();
-            String to = sc.next();
-            move(from, to);
+    public void printBoard() {
+        System.out.println("  a b c d e f g h");
+        for (int i = 0; i < 8; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < 8; j++) {
+                String piece = board[i][j];
+                System.out.print((piece.isEmpty() ? "." : piece) + " ");
+            }
+            System.out.println((8 - i));
         }
+        System.out.println("  a b c d e f g h");
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,43 +1,18 @@
-import javax.swing.*;
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
+        game game = new game();
+        Scanner sc = new Scanner(System.in);
 
-        JFrame frame = new JFrame("Chess");
-        frame.setSize(1920, 1080);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JButton start = new JButton("new game");
-        JButton exit = new JButton("exit");
-
-        JPanel panel = new JPanel();
-        panel.add(start);
-        panel.add(exit);
-
-        frame.add(panel);
-        frame.setVisible(true);
-
-        exit.addActionListener(e -> System.exit(0));
-
-        start.addActionListener(e -> {
-            frame.getContentPane().removeAll();
-
-            game game = new game();
-            map board = new map(game);
-
-            frame.add(board);
-            frame.revalidate();
-            frame.repaint();
-
-            new Thread(() -> {
-                Scanner sc = new Scanner(System.in);
-                while (true) {
-                    System.out.print("Enter the move (a2 a4): ");
-                    game.move(sc.next(), sc.next());
-                    board.repaint();
-                }
-            }).start();
-        });
+        while (true) {
+            game.printBoard();
+            System.out.print("Make a move (For example, e2 e4): ");
+            String from = sc.next();
+            String to = sc.next();
+            game.move(from, to);
+        }
     }
 }
+
